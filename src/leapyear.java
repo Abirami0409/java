8daff117eddadc360f0c871e04cd284084d454fb
Abirami0409 <abiramis4235@gamil.com>
2026-02-14 23:36:34 +0530

    init commit

---
+++ src/leapyear.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class leapyear {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int year = sc.nextInt();
+
+        String result = (year % 4 == 0) ? "Leap Year" : "Not Leap Year";
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}

37f77387caf558d15a989e8ae12d6985724a93ce
Abirami0409 <abiramis4235@gamil.com>
2026-03-03 21:10:13 +0530

    init commit

---
+++ src/array.java
@@ -0,0 +1,23 @@
+public class array {
+    public static void main(String[] args) {
+
+        int arr[] = {4, 67, 23, 45, 90};
+
+        int max = arr[0];
+        int min = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println("Maximum value of the array: " + max);
+        System.out.println("Minimum value of the array: " + min);
+    }
+}
